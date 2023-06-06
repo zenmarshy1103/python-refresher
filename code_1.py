5313b05d642de5a1bcf97353b6bebdcc2e15128a
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-06-06 13:20:13 +1200

    Variables
    - Strings
    - Integers 
    - Float

---
+++ code_1.py
@@ -0,0 +1,27 @@
+# Variables
+# - Strings
+# - Integers 
+# - Float 
+
+# >> Define a variable
+x = 15    # python will create this value and store it in its memory
+
+price = 9.99
+discount = 0.2
+
+result = price * (1 - discount)
+print(result)
+
+print('Hello World')
+
+person1 = "Jason Liu " 
+print(person1)
+
+a = 25
+b = a
+
+print(a, b)
+
+b = 17
+print(a, b)
+
