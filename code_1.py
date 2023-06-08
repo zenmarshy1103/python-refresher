0378f766240f98563ad645d8d6e894bd7cac982c
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-06-09 11:51:08 +1200

    Lists, Tuples and Sets
    # - Lists (element is in order)
    # - Tuples (Not Mute-able)
    # - Sets (Duplicated element is ignored and does not have order)

--- code_1.py
+++ code_1.py
@@ -1,11 +1,58 @@
-# Getting User Input
-# Input value entered will return a string type
+# Lists, Tuples and Sets
+# - Lists (element is in order)
+# - Tuples (Not Mute-able)
+# - Sets (Duplicated element is ignored and does not have order)
 
-# name = input("Please enter you name: ")
-# age = input("Please enter you age: ")
-# print(f"Hi {name}, you are {age} years old")
+#Lists
+# - Subscript is used to get get the position of the element ie l[0] first element etc
+l = [ "Bob", "Rolf", "Anne"]
 
-# Mathematic operation from input
-sizeInput = input("How bis is your house (in squares feet): ")
-squareMeter = int(sizeInput) / 10.8    #converting sting into int (a number)
-print(f"Square feet: {sizeInput} is {squareMeter:.2f} square meters") #Show the number for 2dp (.2f)
+#Tuples
+# Cannot add or remove elements
+# - Subscript is used to get get the position of the element ie l[0] first element etc
+t = ("Bob", "Rolf", "Anne")
+
+#Sets
+# Cannot contain duplicates, ignores the duplicated element
+# - cannot use subscript to get the position of the element
+# - sets do not have order
+s = {"Bob", "Rolf", "Anne", "Anne"}
+print(s)
+
+for element in s:
+    print(element)
+
+
+# > Change value of element
+# - List
+l[2] = "Jason"
+print(l)
+
+# - Tuples (not mute-able)
+# t[0] = "nick"
+# print(t)
+
+# - Sets (does not have subscription so values cannot be changed)
+
+# > Add Element
+# - List
+l.append('Faye')
+print(l)
+
+# - Tuple (not Mute-able)
+
+# - Sets
+s.add("Cyrus")
+print(s)
+
+
+# > Remove Element
+# - List
+l.remove('Jason')
+print(l)
+
+# - Tuple (not Mute-able)
+
+
+
+
