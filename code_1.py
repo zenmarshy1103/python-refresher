e1fa9e8fdbc86e0faaf260998ea69e725b761650
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-07-04 07:41:12 +1200

    Functions
    # Are callable variable that is defined by you
    # Defining our own function
    # Can reuse functions
    # Can use Global and Function Scope Variables

--- code_1.py
+++ code_1.py
@@ -1,49 +1,33 @@
-# Destructing Variables 
+# Functions 
+# Are callable variable that is defined by you
+# Defining our own function
+# Can reuse functions
+# Can use Global and Function Scope Variables
 
-# tuples (The () is not needed but will needed if you put a tuple inside a list)
-x = 5, 11
-print(type(x))
-
-# Destructure a tuple with valuables assigned to each value in the tuple
-x, y = 4, 22
-print(x)
-print(y)
-
-student_attendance = {
-    "Jason": 100,
-    "Cyrus": 100,
-    "Oreo": 50,
-}
-
-#Turning dictionary into a list
-print(list(student_attendance.items()))  # <NOTE> the dictionary items are turned into tuples and stored in the list 
-
-# Iterate through each of the tuples in the list and destructure into three separate categories
-people = [("Bob", 42, "Mechanics"), ("James", 24, "Artest"), ("Harry", 32, "Lecturer")]
-
-for name, age, job in people:  # iterate over people to destructure into name age and job
-    print(f"Name: {name}, Age: {age}, Job: {job}")
+# Function without return 
+# Defining the code
+def hello():
+    print("Hello!")
   
-  
-# Ignore a value in destructuring
-person = ("Jason", 34, "Front End Developer")
-name, _, profession = person    # _ is ignored
-print(name, profession)  
-    
-    
-# Destructure a list
-author = ["Jason", 23]
-name, age = author
-print(name)
-print(age)
+# Running the code    
+hello()
 
-# Destructuring the remaining values
-head, *tail = [1, 2, 3, 4, 5]   # *tail will collect all of the destructuring values into this variable
-print(head)
-print(tail)
+def user_age_in_seconds():
+    user_age = int(input("Enter your age: "))
+    age_seconds = user_age * 365 * 24 * 60 * 60   
+    print(age_seconds)
+    
+user_age_in_seconds()
 
-*head, tail = [5, 4, 3, 2, 1] # *head will collect all the front until the one before the last one
-print(head)
-print(tail)
+# Using global scope varaible 
+friends = ["Jason", "Bob"]  
+
+def add_friend():
+    friend_name = input("Enter your friend's name: ")
+    friends.append(friend_name)
+
+add_friend()    
+print(friends)
+
 
 
