07617e165d612a55545033591261541e26db3ac9
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-07-04 08:34:22 +1200

    Functions - Lambda Functions
    #   - A function that does not have a name but used to return values
    #   - Used to operate on input and return output, never used to perform actions  
    #   - Using a list Comprehension as argument 
    #   - Alternative to using list comprehension as arguments use map()

--- code_1.py
+++ code_1.py
@@ -1,33 +1,28 @@
-# Functions - Returning Values
+# Functions - Lambda Functions
+#   - A function that does not have a name but used to return values
+#   - Used to operate on input and return output, never used to perform actions  
+#   - Using a list Comprehension as argument 
+#   - Alternative to using list comprehension as arguments use map()
 
-# Function without return
-#   - will return None by default
-def add(x, y):
-    print(x + y)
-    
-add(5, 8)
+# Lambda Function
+#   - We do not specify the return keyword
+#   - <SYNTAX>  lambda arg_1, arg_2 : return value
+add = lambda x, y: x + y
 
-# Function with return
-#   - will return the value specified
+print(add(5, 7))
 
-def addition(x, y):
-    return x + y
+def double(x):
+    return x  * 2
 
-sum = addition(5, 8)
-print(sum)
+# Using List Comprehension as the argument
+#   - List Comprehension returns a new list
+sequence = [1, 2, 4, 6]
+doubled = [double(x) for x in sequence]
+print(doubled)
 
-
-def divide(divident, divisor):
-    if divisor !=0:
-        return int(divident / divisor)
-    else:
-        return "you fool!"
-    
-result = divide(15, 3)
-print(result)
-
-
-
-
-
- 
+# Using Map Function on list
+#   - Loop over a list and does a function over a list
+#   - In Python map returns a map object, to use it need to turn in into a list first list(map())
+#   - <SYNTAX> map(function, iterable)
+doubled_map = list(map(double, sequence))
+print(doubled_map)
