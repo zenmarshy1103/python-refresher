c374199365c2267ca0012e41d9e49a3755acc872
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-07-04 09:01:40 +1200

    Dictionary Comprehension
    #   - returns a dictionary with key-value pairs
    #   <SYNTAX>
    #       {key:value for index in list }

--- code_1.py
+++ code_1.py
@@ -1,28 +1,54 @@
-# Functions - Lambda Functions
-#   - A function that does not have a name but used to return values
-#   - Used to operate on input and return output, never used to perform actions  
-#   - Using a list Comprehension as argument 
-#   - Alternative to using list comprehension as arguments use map()
+# Dictionary Comprehension
+#   - returns a dictionary with key-value pairs
+#   <SYNTAX>
+#       {key:value for index in list }
 
-# Lambda Function
-#   - We do not specify the return keyword
-#   - <SYNTAX>  lambda arg_1, arg_2 : return value
-add = lambda x, y: x + y
+# A list with tuples stored in it
+users = [
+    (0, "Bob", "password"),
+    (1, "Rolf", "bob123"),
+    (2, "Jose", "longp4assword"),
+    (3, "username", "1234"),
+]
 
-print(add(5, 7))
+# Dictionary Comprehension: 
+#   - Adding key-Value pairs onto a new dictionary
+#   <SYNTAX>
+#       {key:value for index in list }
 
-def double(x):
-    return x  * 2
+username_mapping = {user[1]: user for user in users} # Getting {"BoB": (0, "Bob", "password")} ...
+print(username_mapping)
 
-# Using List Comprehension as the argument
-#   - List Comprehension returns a new list
-sequence = [1, 2, 4, 6]
-doubled = [double(x) for x in sequence]
-print(doubled)
+# same as 
+mapping_users = {}  # Need to create an empty dictionary to put key-value pairs in
 
-# Using Map Function on list
-#   - Loop over a list and does a function over a list
-#   - In Python map returns a map object, to use it need to turn in into a list first list(map())
-#   - <SYNTAX> map(function, iterable)
-doubled_map = list(map(double, sequence))
-print(doubled_map)
+for user in users:
+    mapping_users[user[1]] = user 
+    
+print(mapping_users)
+
+# REAL-WORLD CASE: Checking login (username and password)    
+
+# Since username_mapping is:
+#{'Bob': (0, 'Bob', 'password'), 
+# 'Rolf': (1, 'Rolf', 'bob123'), 
+# 'Jose': (2, 'Jose', 'longp4assword'), 
+# 'username': (3, 'username', '1234')}
+print(username_mapping.keys())
+
+#inputs for users
+username_input = input("Enter your username: ")
+password_input = input("Enter you password: ")
+
+# check if the user input is contained in the dictionary 
+if username_input in username_mapping.keys():
+    # Destructure the value when calling the key to username and password and ignoring the 1st value
+    _, username, password = username_mapping[username_input]
+
+    if  password_input == password:
+        print("Logging in")
+    else:
+        print("Try again")
+else:
+    # Output this line when the user input is not contained dictionary
+    print("Please inform IT")
