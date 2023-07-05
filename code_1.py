b968e970766f524590dce3221975698a91b160d3
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-07-06 08:50:46 +1200

    Object Oriented Programming - Magic Methods: _ _str _ _ and _ _ repr _ _
    #   - Special Methods: _ _ method _ _ will be called by python automatically at times
    #   - These two method is used for representing an object (Not compulsory to use)

--- code_1.py
+++ code_1.py
@@ -1,50 +1,30 @@
-# functions - Unpacking Keyword Arguments 
-#   - **kwargs unpacks arguments and store them in a kwarg dictionary
-#   - arguments passed in **kwargs must be a dictionary
-#   - ** can be used for destructing(unpack) a dictionary
+# Object Oriented Programming - Magic Methods: _ _str _ _ and _ _ repr _ _
+#   - Special Methods: _ _ method _ _ will be called by python automatically at times
+#   - These two method is used for representing an object (Not compulsory to use)
 
-def named(**kwargs):  # ** collects keyword arguments and put it into a dictionary
-    print(kwargs)
-    
-named(name="jason", age=23)
-
-# Using destructing on the dictionary and turn the properties into the parameters that the function parameter list contain
-def name(name, age):
-    print(name, age)
-
-#   - Dictionary
-details = {
-    "name": "Bob",
-    "age": 25
-}
-
-# destructuring dictionary
-#   <SYNTAX> **dictionary
-
-#calling the name function and place the destructuring dictionary as the argument of the function
-name(**details)  
-
-def print_nicely(**kwargs):
-    #<Note> **kwargs collects the keyword argument and store them in a dictionary called kwarg
-    named(**kwargs) # here the argument to the calling function destructure the kwarg dictionary and passed in the named arguments
-    # Destructuring the kwarg (dictionary) into arg and value and print them out
-    for arg, value in kwargs.items(): # Kwarg here gets the dictionary collected by the **kwargs from the print_nicely parameter list
-        print(f"{arg}: {value}")
+class Person:
+    def __init__(self, name, age): # Special (Magic) Method
+        # The method gives us an empty object called self
         
-print_nicely(named="Bob", age=25)
-
-
-def both(*args, **kwargs):
-    #   - *args collects all the positional arguments and store them in a arg tuple
-    print(args)
-    #   - **kwargs collects all the named arguments and store them in a kwarg dictionary
-    print(kwargs)
-
-both(1, 3, 5, name="Jason", age=25)
-
-def myfunction(**kwargs):
-    print(kwargs)
+        #setting the name and age properties of the empty object (self)
+        self.name = name 
+        self.age = age
+        
+    # A special method that turns your object into a string representation
+    #   - To print out nice and easy to read string to users     
+    # def __str__(self):
+    #     return f"This Person {self.name}, {self.age} years old"
     
-# <REMEMBER>
-#   what is passed into **kwarg must be a dictionary of it will come up with an error
+    # A special method that is used to tell programers to read the string here that we are printing out an object
+    #   - Should return a string that allows us to recreate the original object with ease
+    #   - to display this make sure the __str__ method is commented or not present or python will print out the __str__ not the __repr__ method
+    def __repr__(self):
+        return f"<Person('{self.name}', {self.age})>"
+        
+        
+        
+    
+jason = Person("Jason", 34)  # When creating an object from the class, python calls the __init__ method even when you didn't call it
+print(jason)
+
 
